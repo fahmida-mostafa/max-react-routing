27a90d396366cb4508c4b8906438e89550f47adb
Fahmida Mostafa <fahmidamostafa@Fahmidas-MacBook-Air.local>
2021-03-06 17:50:50 -0500

    first commit - no.198, 2min

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import './Blog.css';
+import NewPost from './NewPost/NewPost';
+import Posts from './Posts/Posts';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#id',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
+            </div>
+        );
+    }
+}
+
+export default Blog;
